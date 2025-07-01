b4b344fa58b8d9086f00483de5d8d1257a053222
One Shine <117028965+wannn28@users.noreply.github.com>
2025-07-01 16:35:06 +0700

    fix(blood_donations): update image validation rules in donation requests
    - Removed file type restriction from the image field in BloodDonationCreateRequest.
    - Ensured image field in BloodDonationUpdateRequest is optional with 'omitempty' validation.

--- DarahConnectAPI/internal/http/dto/blood_donations.go
+++ DarahConnectAPI/internal/http/dto/blood_donations.go
@@ -12,14 +12,14 @@ type BloodDonationCreateRequest struct {
 	DonationDate   time.Time             `json:"donation_date" form:"donation_date" validate:"required"`
 	BloodType      string                `json:"blood_type" form:"blood_type" validate:"required"` // e.g., "A+", "O-", etc.
 	Status         string                `json:"status" form:"status" validate:"required"`
-	Image          *multipart.FileHeader `json:"image" form:"image" validate:"required,ext=jpg|jpeg|png"`
+	Image          *multipart.FileHeader `json:"image" form:"image" validate:"required"`
 }
 
 type BloodDonationUpdateRequest struct {
 	Id           int64                 `param:"id" validate:"required"`
 	DonationDate time.Time             `json:"donation_date" form:"donation_date"`
 	BloodType    string                `json:"blood_type" form:"blood_type"` // e.g., "A+", "O-", etc.
-	Status       string                `json:"status" form:"status"`     //'Completed', 'Rejected', 'Deferred'
+	Status       string                `json:"status" form:"status"`         //'Completed', 'Rejected', 'Deferred'
 	Image        *multipart.FileHeader `form:"image" validate:"omitempty"`
 }
 
